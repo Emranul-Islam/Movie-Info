f1f08e82c0b982932d1b48fb0fde45ba7d09f607
emranul-islam <emranul.islam.sohag@gmail.com>
2020-10-01 00:48:16 +0600

    created

---
+++ app/src/main/java/com/emranul/movieinfo/MainActivity.java
@@ -0,0 +1,63 @@
+package com.emranul.movieinfo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.emranul.movieinfo.fragment.HomeFragment;
+import com.emranul.movieinfo.fragment.RankFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        frameLayout = findViewById(R.id.frame);
+        setSupportActionBar(toolbar);
+
+        loadFragment(new HomeFragment(),"Home");
+        BottomNavigationView nav = findViewById(R.id.bottom_nav);
+        nav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId()) {
+                case R.id.item_home:
+
+                    loadFragment(new HomeFragment(),"Home");
+                    return true;
+                case R.id.item_rank:
+                    loadFragment(new RankFragment(),"Rank");
+                    return true;
+                case R.id.item_find:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private void loadFragment(Fragment fragment, String title) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame,fragment)
+                .addToBackStack(null)
+                .commit();
+        toolbar.setTitle(title);
+    }
+}
